0473a053136134a2b374bb3cd21a48207eccd1aa
= <=>
2015-12-17 19:59:29 +0900

    修正

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -7,130 +7,139 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-
 public class KisoKadai3 {
 
 	public static void main(String[] args) throws IOException {
 //スタート
+		boolean t = true;
+		boolean t2 = true;
+		String str,str2;
+		String str3,str4;
+		int a = 0;
+
+
+		while(t){
+			System.out.println("該当する数字を入力して下さい  1.フォルダ・ファイルの新規作成 2.ファイルの読み込み 3.ファイルへの書き込み 4.終了");
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int a,b = 0;
-		boolean z = true;
-		boolean z1 = true;
-		boolean z2 = true;
-		String str,str1;
-		String str2,str3;
-		FileWriter  fw;
-		PrintWriter pw;
+		try{str = br.readLine();
+		 a = Integer.parseInt(str);}
+		 catch(NumberFormatException nfe){continue;}
 
-		while(z){
-		System.out.println("文字入力スペース");
-		str = br.readLine();
-//分岐
-		System.out.println("今の記述を保存しますか？");
-			while(z1){
-				System.out.println("該当する数字を入力して下さい 1.保存しない 2.新規フォルダ・ファイルの作成、保存"
-				+ " 3.既存のフォルダ・ファイルに保存 4.ファイルの参照 5.文字入力");
+		 switch(a){
+		  case 1 :// 1.フォルダ・ファイルの新規作成
+			   while(t2){
+				  System.out.println("新規フォルダ・ファイルの作成をします"+"\n"+"1.新規フォルダ作成 2.既存ファイルに新規ファイルを作成");
+				   try{str = br.readLine();
+				   a = Integer.parseInt(str);}
+				   catch(NumberFormatException nfe){continue;}
+				   if(a == 1){
+					   System.out.println("フォルダ名を指定して下さい 「c:\\\\」は不要");//新規フォルダ作成
+					   str = br.readLine();
+					   File f = new File("c:\\\\" + str);
+					 		if(!f .exists()){f .mkdirs();
+					 			System.out.println("フォルダを作成しました"+"\n"+"ファイル名を入力して下さい「ｃ:\\\\」「.txt」は不要");
+					 		}else{System.out.println("フォルダは既に存在する為、失敗しました");continue;}
+					 			try{str2 = br.readLine();
+					 			File newfile = new File("c:\\\\" + str + "\\\\" + str2 + ".txt");
+					 				if(newfile.createNewFile()){
+					 					try{FileWriter fw  = new FileWriter("c:\\\\" + str + "\\\\" + str2 + ".txt");
+					 					 PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+					 					 pw.println(str);
+					 					 pw.close();}
+					 					catch (IOException ex) {ex.printStackTrace();continue;}
+					 					System.out.println("ファイルを作成し、書き込みました"+"\n"+"Enterを押して下さい");break;
+								    }else{}
+					 			}catch (IOException e){System.out.println(e);}
+				  }
 
-				try{str1 = br.readLine();
-				a = Integer.parseInt(str1);}
-				catch(NumberFormatException nfe){continue;}
+				   else if(a == 2){
+					   System.out.println("既存のフォルダ名を指定して下さい 「c:\\\\」は不要");////新規ファイル作成
+					   str = br.readLine();
+					   File f = new File("c:\\\\" + str);
+					 		if(!f .exists()){System.out.println("そのフォルダは存在しない為、失敗しました");continue;
+					 		}else{System.out.println("フォルダは存在します。作成するファイル名を入力して下さい");}
+					 		try{str2 = br.readLine();
+				 			File newfile = new File("c:\\\\" + str + "\\\\" + str2 + ".txt");
+				 				if(newfile.createNewFile()){
+				 					try{FileWriter fw  = new FileWriter("c:\\\\" + str + "\\\\" + str2 + ".txt");
+				 					 PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+				 					 pw.println(str);
+				 					 pw.close();}
+				 					catch (IOException ex) {ex.printStackTrace();continue;}
+				 					System.out.println("ファイルを作成し、書き込みました");break;
+							    }else{System.out.println("ファイルは既に存在している為、作成に失敗しました");}
+				 			}catch (IOException e){System.out.println(e);}
+				   	}
+				}break;
 
-			if(a == 1){//保存しない場合
-					System.out.println("テキストファイルを終了しますか？ 1.いいえ 2.はい");
-					try{str2 = br.readLine();
-					b = Integer.parseInt(str2);}
-					catch(NumberFormatException nfe){continue;}
-						if(b == 1){continue;}//メニュー選択にとばす　まだ出来ていない。
-						else{}break;}//テキスト終了　○
+		  case 2 ://2.ファイルの読み込み
+			  while(t2){
+					System.out.println("読み込みをしたいファイルの、フォルダ名を入力して下さい 「c:\\\\」は不要");
+					br  = new BufferedReader(new InputStreamReader(System.in));
+					str = br.readLine();
+					 File f = new File("c:\\\\" + str);
+					 	if(!f.exists()){
+					 		System.out.println("フォルダは存在しませんでした");continue;}
+					 	else{System.out.println("フォルダは存在します"+"\n"+"読み込みをしたいファイル名を入力して下さい 「ｃ:\\\\フォルダ名\\\\」「.txt」は不要");}
+						str2 = br.readLine();
+						 try{FileReader fr = new FileReader("c:\\\\"+ str +"\\\\"+ str2 +".txt");
+						  br = new BufferedReader(fr);
+						  String line;
+						  int count = 0;
+						  while((line = br.readLine()) != null){System.out.println(++count +"行目:" + line);}
+						  br.close();
+						  fr.close();break;
+						  }catch (IOException e){e.printStackTrace();
+						  System.out.println("ファイルが存在しない為、失敗しました");}
 
-			else if(a == 2){//新規ファイル・フォルダの作成
-				System.out.println("新規フォルダ・ファイルの作成をします");
-				//フォルダ
-					System.out.println("フォルダ名を指定して下さい 「c:\\\\」は不要");
-					 str2 = br.readLine();
-					 File f = new File("c:\\\\" + str2);
-					 	if(!f .exists()){f .mkdirs();
-					 		System.out.println("フォルダを作成しました"+"\n"+"ファイル名を指定してください 「ｃ:\\\\」「.txt」は不要");
-					 	}else{System.out.println("フォルダは既に存在します"+"\n"+"新しいファイル名を指定してください 「ｃ:\\\\」「.txt」は不要");}
-					 		try{str3 = br.readLine();
-							File newfile = new File("c:\\\\" + str2 + "\\\\" + str3 + ".txt");
-								if(newfile.createNewFile()){
-								try{fw = new FileWriter("c:\\\\" + str2 + "\\\\" + str3 + ".txt");
-									pw = new PrintWriter(new BufferedWriter(fw));
-									pw.println(str);
-									pw.close();}
-								catch (IOException ex) {ex.printStackTrace();break;}
-								System.out.println("ファイルを作成し、書き込みました"+"\n"+"Enterを押して下さい");z2 = false;
-								}else{System.out.println("ファイルは既に存在しています");continue;}
-							}catch (IOException e){System.out.println(e);}
-						 }
+			}break;
 
-			else if(a == 3){//既存のフォルダ・ファイルに保存
-				System.out.println("保存するフォルダ名を入力してください 「c:\\\\」は不要");
-				str2 = br.readLine();
-				 File f = new File("c:\\\\" + str2);
-				 	while(z2){
-				 if(f .exists()){
-				 		System.out.println("フォルダは存在します"+"\n"+"追加と上書きどちらにしますか？ 1.追加 2.上書き");break;}
-				 	else{System.out.println("フォルダは存在しません");}}
-				 	if(b == 1){//追加
-						try{str2 = br.readLine();
-						b = Integer.parseInt(str2);}
-						catch(NumberFormatException nfe){continue;}
-						System.out.println("ファイル名を指定して下さい 「ｃ:\\\\」「.txt」は不要");
-						try{str3 = br.readLine();
-						fw = new FileWriter("c:\\\\"+ str2 +"\\\\"+ str3 +".txt",true);
-						pw = new PrintWriter(new BufferedWriter(fw));
-						pw.println(str);
-						pw.close();
-						if(f.exists()){
-						System.out.println("ファイルに追加しました");}
-						else{System.out.println("ファイルは存在しません");}
-						}catch (IOException e){System.out.println(e);}
-					}else{//上書き
-						System.out.println("ファイル名を指定して下さい");}
-						try{str2 = br.readLine();
-						fw = new FileWriter("c:\\\\"+ str2 +"\\\\"+".txt",false);
-						pw = new PrintWriter(new BufferedWriter(fw));
-						pw.println(str);
-						pw.close();
-						if(f.exists()){
-						System.out.println("ファイルを上書きしました");}
-						else{System.out.println("ファイルは存在しません");}
-						}catch (IOException e){System.out.println(e);}
-			}
+		  case 3 ://3.ファイルへの書き込み
+			  while(t2){
+					System.out.println("書き込みをしたいファイルの、フォルダ名を入力して下さい 「c:\\\\」は不要");
+					br  = new BufferedReader(new InputStreamReader(System.in));
+					str = br.readLine();
+					 File f = new File("c:\\\\" + str);
+					 	if(!f.exists()){
+					 		System.out.println("フォルダは存在しませんでした");continue;}
+					 	else{System.out.println("フォルダは存在します"+"\n"+"書き込みをしたいファイル名を入力して下さい 「ｃ:\\\\フォルダ名\\\\」「.txt」は不要");}
+						str2 = br.readLine();
+						 try{FileReader fr = new FileReader("c:\\\\"+ str +"\\\\"+ str2 +".txt");
+						  br = new BufferedReader(fr);
+						  br.close();
+						  fr.close();
+						  System.out.println("ファイルは存在します 書き込む内容を入力して下さい");
+						  }catch (IOException e){e.printStackTrace();
+						  System.out.println("ファイルが存在しない為、失敗しました");continue;}
 
-			else if(a == 4){//ファイルの参照
-				System.out.println("参照したいファイルのフォルダ名を入力して下さい 「c:\\\\」は不要");
-				str2 = br.readLine();
-				 File f = new File("c:\\\\" + str2);
-				 	if(f.exists()){
-				 		System.out.println("フォルダは存在します"+"\n"+"参照したいファイル名を入力して下さい 「ｃ:\\\\」「.txt」は不要");}
-				 	else{System.out.println("フォルダは存在しません");}
-					str3 = br.readLine();
-					try{FileReader fr = new FileReader("c:\\\\"+ str2 +"\\\\"+ str3 +".txt");
-					br = new BufferedReader(fr);
-					String line;
-					int count = 0;
-					while((line = br.readLine()) != null){System.out.println(++count +"行目:" + line);}
-					br.close();
-					fr.close();}
-					catch (IOException ex2){ex2.printStackTrace();
-					System.out.println("フォルダは存在しません");}
-			}
+						 br  = new BufferedReader(new InputStreamReader(System.in));
 
-			else{//文字入力
-				break;
-			}
+						 str3 = br.readLine();
+						 System.out.println("入力した内容はファイルにどう保存しますか？ 1.追記 2.上書き");
+						  try{str4 = br.readLine();
+						  a = Integer.parseInt(str4);}
+						  catch(NumberFormatException nfe){continue;}
+						  	if(a == 1){//追記
+						  	 FileWriter fw = new FileWriter("c:\\\\"+ str +"\\\\"+ str2 +".txt",true);
+						  	 PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+						  	 pw.println(str3);
+						  	 pw.close();
+						  	 System.out.println("ファイルに追加しました");break;
+						  }else{//上書き
+							  FileWriter fw = new FileWriter("c:\\\\"+ str +"\\\\"+ str2 +".txt",false);
+							  PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+							  pw.println(str3);
+							  pw.close();
+							  System.out.println("ファイルを上書きしました");break;}
 
-			System.out.println("プログラムを終了しますか？ 1. いいえ 2.はい");
-			try{str = br.readLine();
-			a = Integer.parseInt(str);}
-			catch(NumberFormatException nfe){continue;}
-				if(a == 1){continue;}//メニュー選択にとばす　まだ出来ていない。
-				else{}break;}break;}
-			}
+				}break;
 
-			}
+		 default ://終了
+			 t = false;
+		 }
 
+	   }
+	}
+}
