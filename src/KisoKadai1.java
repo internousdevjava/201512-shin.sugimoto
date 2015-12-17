279c312482a81da32f322960934d6a89de542b7c
= <=>
2015-12-17 14:12:43 +0900

    はい

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,14 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class KisoKadai1 {
 
-	public static void main(String[] args){
-				for(int x=1;x<=10;x++){
-			for(int y=1;y<=10;y++){
-				if(x*y <= 9){
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str;
+		int x = 0,y = 0;
+		boolean kuku = true;
+
+	while(kuku){
+		try{System.out.print("1~10の間の整数を入力して下さい: x= ");
+		str = br.readLine();
+		 x= Integer.parseInt(str);
+		 if(x <= 10){
+		System.out.print("もうひとつ、1~10の間の整数を入力して下さい: y= ");
+		 }else{System.out.println("やり直してください");continue;}
+		str = br.readLine();
+		y= Integer.parseInt(str);
+		if(y <=10){
+			break;}
+		else{System.out.println("やり直してください");}}
+		catch(NumberFormatException nfe){}}
+
+		for(int i=1 ; i<=x; i++){
+			for(int j =1; j<=y; j++){
+				if(i*j <= 9){
 				System.out.print("  ");
-				}else if(x*y <= 99){
+				}else if(i*j <= 99){
 					System.out.print(" ");
 				}else{}
-		System.out.print(x*y);}
+
+
+		System.out.print(i*j);}
 		System.out.println("");}
-		}
+	}
 	}
